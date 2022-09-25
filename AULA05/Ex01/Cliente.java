03930364d45b92d61c49d3a084a240c57fe5c00f
atguilherme <87152028+atguilherme@users.noreply.github.com>
2022-09-24 22:41:03 -0300

    Add files via upload

---
+++ AULA05/Ex01/Cliente.java
@@ -0,0 +1,10 @@
+package Aula05.Ex01;
+
+public class Cliente {
+
+		public int id;
+		public String nome;
+		public int idade;
+		public String email;
+		
+}
---
+++ AULA05/Ex02/Produto.java
@@ -0,0 +1,12 @@
+package Aula05.Ex02;
+
+public class Produto {
+
+	public int id;
+	public String descricao;
+	public double valor;
+	public double quantidade;
+	
+	
+	
+}
